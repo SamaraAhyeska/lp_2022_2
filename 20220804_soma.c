1eb87a06991070d9eb24cf7c12da1682be21ecca
jeansousaiftm <61210073+jeansousaiftm@users.noreply.github.com>
2022-08-04 22:18:01 -0300

    Add files via upload

---
+++ 20220804_soma.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main() {
+
+    int x, y, soma;
+
+    printf("Digite a variavel X: ");
+    scanf("%d", &x);
+    printf("Digite a variavel Y: ");
+    scanf("%d", &y);
+
+    soma = x + y;
+
+    printf("A soma e %d", soma);
+
+    return 0;
+}
---
+++ 20220804_subtracao.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main() {
+    int n1, n2, sub;
+    scanf("%d %d", &n1, &n2);
+    sub = n1 - n2;
+    printf("%d\n", sub);
+    return 0;
+}
